1090dc685487e6426043b7bf83708277395d0ffc
Wedge-Dev <wedgeontour@hotmail.com>
2021-02-24 19:21:45 +0100

    Week 5 code (optional)

---
+++ Crash Course on Python/Week 5/Classes & Methods/Constructors and Other Special Methods.py
@@ -0,0 +1,16 @@
+#In this code, there's a Person class that has an attribute name, which gets set when constructing the object.
+#Fill in the blanks so that
+#1) when an instance of the class is created, the attribute gets set correctly, and
+#2) when the greeting() method is called, the greeting states the assigned name
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def greeting(self):
+        # Should return "hi, my name is " followed by the name of the Person.
+        return "hi, my name is {}".format(self.name)
+
+# Create a new instance with a name of your choice
+greg = Person("Greg")
+#greg.name = "Greg_1"
+print(greg.greeting())
